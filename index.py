52fdebe10c217b6e02b45ba935dcfffe44343ef6
Kishore Kumar A V <kishore.kumar@techolution.com>
2025-10-24 00:56:23 +0530

    commit 1

--- index.py
+++ index.py
@@ -1,21 +1,7 @@
 from flask import Flask, request, make_response, render_template_string
 
 app = Flask(__name__)
-TEMPLATE = '''
-<!doctype html>
-<html>
-    <head><title>Sudoku Game</title></head>
-    <body>
-        <h1>Welcome to Web Sudoku</h1>
-        <form method="POST">
-            Row: <input name="row" type="number" min="0" max="8"><br>
-            Column: <input name="col" type="number" min="0" max="8"><br>
-            Number: <input name="num" type="number" min="1" max="9"><br>
-            <input type="submit" value="Submit Move">
-        </form>
-    </body>
-</html>
-'''
+
 @app.route("/", methods=["GET", "POST"])
 def sudoku_web():
     resp = make_response(render_template_string(TEMPLATE))  # ✅ Step 1: make_response used
