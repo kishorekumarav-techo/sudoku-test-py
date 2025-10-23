702f418cc46abd803f75305cb21b54985473294c
Kishore Kumar A V <kishore.kumar@techolution.com>
2025-10-24 01:04:12 +0530

    fixed few violations

--- index.py
+++ index.py
@@ -1,12 +1,25 @@
 from flask import Flask, request, make_response, render_template_string
 
 app = Flask(__name__)
-
+TEMPLATE = '''
+<!doctype html>
+<html>
+    <head><title>Sudoku Game</title></head>
+    <body>
+        <h1>Welcome to Web Sudoku</h1>
+        <form method="POST">
+            Row: <input name="row" type="number" min="0" max="8"><br>
+            Column: <input name="col" type="number" min="0" max="8"><br>
+            Number: <input name="num" type="number" min="1" max="9"><br>
+            <input type="submit" value="Submit Move">
+        </form>
+    </body>
+</html>
+'''
 @app.route("/", methods=["GET", "POST"])
 def sudoku_web():
     resp = make_response(render_template_string(TEMPLATE))  # ✅ Step 1: make_response used
 
-    # ❌ Step 2 + Step 3: .set_cookie is called without secure=True
-    resp.set_cookie("sudoku_session", "fake-session-id-12345")
+    resp.set_cookie("sudoku_session", os.getenv("sudoku_session", ""))
 
     return resp
--- sudoku.py
+++ sudoku.py
@@ -78,30 +78,6 @@ from flask import Flask, request, make_response, render_template_string
 
 app = Flask(__name__)
 
-# Simple HTML template to simulate Sudoku interface
-TEMPLATE = '''
-<!doctype html>
-<html>
-    <head><title>Sudoku Game</title></head>
-    <body>
-        <h1>Welcome to Web Sudoku</h1>
-        <form method="POST">
-            Row: <input name="row" type="number" min="0" max="8"><br>
-            Column: <input name="col" type="number" min="0" max="8"><br>
-            Number: <input name="num" type="number" min="1" max="9"><br>
-            <input type="submit" value="Submit Move">
-        </form>
-    </body>
-</html>
-'''
-
-@app.route("/", methods=["GET", "POST"])
-def sudoku_web():
-    resp = make_response(render_template_string(TEMPLATE))
-
-    resp.set_cookie("sudoku_session", "fake-session-id-12345")
-
-    return resp
 
 if __name__ == "__main__":
     sudoku_game()
